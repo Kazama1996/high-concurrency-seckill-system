1db1984a26bc93663cf63111b7af3e9263b5e74e
Kazama1996 <y.kazama1996@gmail.com>
2026-03-25 23:18:50 +0800

    refactor: move CircuitBreakerConfig to their respective domain packages

--- src/main/java/com/kazama/redis_cache_demo/product/config/CircuitBreakerConfig.java
+++ src/main/java/com/kazama/redis_cache_demo/product/config/CircuitBreakerConfig.java
@@ -1,17 +1,16 @@
-package com.kazama.redis_cache_demo.order.config;
+package com.kazama.redis_cache_demo.product.config;
+
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration("OrderCircuitBreakerConfig")
+@Configuration("ProductCircuitBreakerConfig")
 public class CircuitBreakerConfig {
 
-
-    @Bean("orderDBCircuitBreaker")
-    public CircuitBreaker orderDBCircuitBreaker(CircuitBreakerRegistry registry){
-        return registry.circuitBreaker("orderDB");
+    @Bean("productDBCircuitBreaker")
+    public CircuitBreaker productDBCircuitBreaker(CircuitBreakerRegistry registry){
+        return registry.circuitBreaker("productDB");
     }
-
 }
